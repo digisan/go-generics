fd459964afb161170562b43bb8d961bd17cf80ae
Qing Miao <cdutwhu@outlook.com>
2022-03-25 13:32:04 +1100

    * add some func to v2, TODO... adding more

--- v2/generics.go
+++ v2/generics.go
@@ -1,9 +1,14 @@
 package v2
 
+import (
+	"reflect"
+	"unsafe"
+)
+
 type Ordered interface {
-	int | int8 | int16 | int32 | int64 |
-		uint | uint8 | uint16 | uint32 | uint64 |
-		uintptr | float32 | float64 | string
+	~int | ~int8 | ~int16 | ~int32 | ~int64 |
+		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 |
+		~uintptr | ~float32 | ~float64 | ~string
 }
 
 func Max[T Ordered](arr ...T) T {
@@ -19,6 +24,20 @@ func Max[T Ordered](arr ...T) T {
 	return m
 }
 
+func MaxIdx[T Ordered](arr ...T) (T, int) {
+	if len(arr) == 0 {
+		panic("Max args at least has one element")
+	}
+	idx, m := 0, arr[0]
+	for i, a := range arr[1:] {
+		if a > m {
+			m = a
+			idx = i + 1
+		}
+	}
+	return m, idx
+}
+
 func Min[T Ordered](arr ...T) T {
 	if len(arr) == 0 {
 		panic("Min args at least has one element")
@@ -30,4 +49,149 @@ func Min[T Ordered](arr ...T) T {
 		}
 	}
 	return m
+}
+
+func MinIdx[T Ordered](arr ...T) (T, int) {
+	if len(arr) == 0 {
+		panic("Min args at least has one element")
+	}
+	idx, m := 0, arr[0]
+	for i, a := range arr[1:] {
+		if a < m {
+			m = a
+			idx = i + 1
+		}
+	}
+	return m, idx
+}
+
+// IdxOf : returns the index of the first instance of e in slice, or -1 if e is not present in slice
+func IdxOf[T comparable](e T, arr ...T) int {
+	for i, ele := range arr {
+		if ele == e {
+			return i
+		}
+	}
+	return -1
+}
+
+// LastIdxOf : returns the index of the last instance of e in slice, or -1 if e is not present in slice
+func LastIdxOf[T comparable](e T, arr ...T) int {
+	for i := len(arr) - 1; i >= 0; i-- {
+		if arr[i] == e {
+			return i
+		}
+	}
+	return -1
+}
+
+// In : if arr has element e, return true. otherwise false
+func In[T comparable](e T, arr ...T) bool {
+	return IdxOf(e, arr...) != -1
+}
+
+// NotIn : if arr does NOT have element e, return true. otherwise false
+func NotIn[T comparable](e T, arr ...T) bool {
+	return !In(e, arr...)
+}
+
+func DelEleOrderlyAt[T comparable](arr *[]T, i int) {
+	if i >= 0 && i < len(*arr) {
+		*arr = append((*arr)[:i], (*arr)[i+1:]...)
+	}
+}
+
+func DelEleAt[T comparable](arr *[]T, i int) {
+	if i >= 0 && i < len(*arr) {
+		(*arr)[i] = (*arr)[len(*arr)-1]
+		(*reflect.SliceHeader)(unsafe.Pointer(arr)).Len--
+	}
+}
+
+func DelOneEle[T comparable](arr *[]T, ele T) {
+	if idx := IdxOf(ele, (*arr)...); idx >= 0 {
+		DelEleAt(arr, idx)
+	}
+}
+
+func DelOneEleOrderly[T comparable](arr *[]T, ele T) {
+	if idx := IdxOf(ele, (*arr)...); idx >= 0 {
+		DelEleOrderlyAt(arr, idx)
+	}
+}
+
+// Settify : remove repeated elements in arr
+func Settify[T comparable](arr ...T) (set []T) {
+	if len(arr) == 0 {
+		return arr
+	}
+	set = make([]T, 0, len(arr))
+	m := make(map[T]struct{})
+	for _, ele := range arr {
+		if _, ok := m[ele]; !ok {
+			set = append(set, ele)
+			m[ele] = struct{}{}
+		}
+	}
+	if len(set) == 0 {
+		return []T{}
+	}
+	return
+}
+
+func Filter[T comparable](data *[]T, check func(i int, e T) bool) []T {
+	if check == nil {
+		return *data
+	}
+
+	p := *data
+	var k = 0
+	for i, v := range p {
+		if check(i, v) {
+			p[k], p[i] = p[i], p[k]
+			k++
+		}
+	}
+	(*reflect.SliceHeader)(unsafe.Pointer(data)).Len = k
+	return p[:k]
+}
+
+// FilterMap : Filter & Modify []string slice, return []string slice
+// func FilterMap[T1, T2 comparable](arr []T1, filter func(i int, e T1) bool, mapper func(i int, e T1) T2) (r []T2) {
+// 	switch {
+// 	case filter != nil && mapper != nil:
+// 		for i, e := range arr {
+// 			if filter(i, e) {
+// 				r = append(r, mapper(i, e))
+// 			}
+// 		}
+// 	case filter != nil && mapper == nil:
+// 		for i, e := range arr {
+// 			if filter(i, e) {
+// 				r = append(r, e)
+// 			}
+// 		}
+// 	case filter == nil && mapper != nil:
+// 		for i, e := range arr {
+// 			r = append(r, mapper(i, e))
+// 		}
+// 	default:
+// 		return arr
+// 	}
+// 	return
+// }
+
+func MapFilter[T1 comparable, T2 any](m map[T1]T2, filter func(k T1, v T2) bool) map[T1]T2 {
+	rt := make(map[T1]T2)
+	for k, v := range m {
+		if filter(k, v) {
+			rt[k] = v
+		}
+	}
+	return rt
+}
+
+// MapCopy:
+func MapCopy[T1 comparable, T2 any](m map[T1]T2) map[T1]T2 {
+	return MapFilter(m, func(k T1, v T2) bool { return true })
 }
--- v2/generics_test.go
+++ v2/generics_test.go
@@ -1,25 +1,28 @@
 package v2
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 )
 
 func TestMaxMin(t *testing.T) {
 	{
+		type f32 float32
+
 		type args struct {
-			arr []float32
+			arr []f32
 		}
 		tests := []struct {
 			name string
 			args args
-			want float32
+			want f32
 		}{
 			// TODO: Add test cases.
 			{
 				name: "OK",
 				args: args{
-					arr: []float32{3, 1, 2, 6, 7, 4, 5, 2, 5},
+					arr: []f32{3, 1, 2, 6, 7, 4, 5, 2, 5},
 				},
 				want: 7,
 			},
@@ -57,5 +60,301 @@ func TestMaxMin(t *testing.T) {
 				}
 			})
 		}
+	}
+}
+
+func TestMaxMinIdx(t *testing.T) {
+	{
+		type args struct {
+			arr []int
+		}
+		tests := []struct {
+			name  string
+			args  args
+			want  int
+			want1 int
+		}{
+			// TODO: Add test cases.
+			{
+				name: "OK",
+				args: args{
+					arr: []int{3},
+				},
+				want:  3,
+				want1: 0,
+			},
+		}
+		for _, tt := range tests {
+			t.Run(tt.name, func(t *testing.T) {
+				got, got1 := MaxIdx(tt.args.arr...)
+				if !reflect.DeepEqual(got, tt.want) {
+					t.Errorf("MaxIdx() got = %v, want %v", got, tt.want)
+				}
+				if got1 != tt.want1 {
+					t.Errorf("MaxIdx() got1 = %v, want %v", got1, tt.want1)
+				}
+			})
+		}
+	}
+	{
+		type args struct {
+			arr []float64
+		}
+		tests := []struct {
+			name  string
+			args  args
+			want  float64
+			want1 int
+		}{
+			// TODO: Add test cases.
+			{
+				name: "OK",
+				args: args{
+					arr: []float64{3, 1, 2, 6, 7, 4, 5, 2, 5},
+				},
+				want:  1,
+				want1: 1,
+			},
+		}
+		for _, tt := range tests {
+			t.Run(tt.name, func(t *testing.T) {
+				got, got1 := MinIdx(tt.args.arr...)
+				if !reflect.DeepEqual(got, tt.want) {
+					t.Errorf("MinIdx() got = %v, want %v", got, tt.want)
+				}
+				if got1 != tt.want1 {
+					t.Errorf("MinIdx() got1 = %v, want %v", got1, tt.want1)
+				}
+			})
+		}
+	}
+}
+
+func TestIdxOf(t *testing.T) {
+	{
+		type args struct {
+			e   int
+			arr []int
+		}
+		tests := []struct {
+			name string
+			args args
+			want int
+		}{
+			// TODO: Add test cases.
+			{
+				name: "",
+				args: args{
+					e:   3,
+					arr: []int{2, 6, 1, 4, 3, 2, 3, 7, 9},
+				},
+				want: 4,
+			},
+		}
+		for _, tt := range tests {
+			t.Run(tt.name, func(t *testing.T) {
+				if got := IdxOf(tt.args.e, tt.args.arr...); got != tt.want {
+					t.Errorf("IdxOf() = %v, want %v", got, tt.want)
+				}
+			})
+		}
+	}
+	{
+		type args struct {
+			e   int
+			arr []int
+		}
+		tests := []struct {
+			name string
+			args args
+			want int
+		}{
+			// TODO: Add test cases.
+			{
+				name: "",
+				args: args{
+					e:   3,
+					arr: []int{2, 6, 1, 4, 3, 2, 3, 7, 9},
+				},
+				want: 6,
+			},
+		}
+		for _, tt := range tests {
+			t.Run(tt.name, func(t *testing.T) {
+				if got := LastIdxOf(tt.args.e, tt.args.arr...); got != tt.want {
+					t.Errorf("LastIdxOf() = %v, want %v", got, tt.want)
+				}
+			})
+		}
+	}
+}
+
+func TestDelEleOrderlyAt(t *testing.T) {
+	type args struct {
+		arr *[]string
+		i   int
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				arr: &[]string{"a", "b", "c", "d", "e"},
+				i:   4,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			DelEleOrderlyAt(tt.args.arr, tt.args.i)
+			fmt.Println(*tt.args.arr)
+		})
+	}
+}
+
+func TestDelEleAt(t *testing.T) {
+	type args struct {
+		arr *[]string
+		i   int
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				arr: &[]string{"a", "b", "c", "d", "e"},
+				i:   4,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			DelEleAt(tt.args.arr, tt.args.i)
+			fmt.Println(*tt.args.arr)
+		})
+	}
+}
+
+func TestDelOneEle(t *testing.T) {
+	type args struct {
+		arr *[]string
+		ele string
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				arr: &[]string{"a", "b", "c", "d", "e"},
+				ele: "b",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			DelOneEle(tt.args.arr, tt.args.ele)
+			fmt.Println(*tt.args.arr)
+		})
+	}
+}
+
+func TestDelOneEleOrderly(t *testing.T) {
+	type args struct {
+		arr *[]string
+		ele string
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				arr: &[]string{"a", "b", "c", "d", "e"},
+				ele: "b",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			DelOneEleOrderly(tt.args.arr, tt.args.ele)
+			fmt.Println(*tt.args.arr)
+		})
+	}
+}
+
+func TestSettify(t *testing.T) {
+	type args struct {
+		arr []string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantSet []string
+	}{
+		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				arr: []string{"a", "b", "a", "c", "d", "c", "e", "b"},
+			},
+			wantSet: []string{"a", "b", "c", "d", "e"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotSet := Settify(tt.args.arr...); !reflect.DeepEqual(gotSet, tt.wantSet) {
+				t.Errorf("Settify() = %v, want %v", gotSet, tt.wantSet)
+			}
+		})
+	}
+}
+
+func TestMapFilter(t *testing.T) {
+	type args struct {
+		m      map[int]string
+		filter func(k int, v string) bool
+	}
+	tests := []struct {
+		name string
+		args args
+		want map[int]string
+	}{
+		// TODO: Add test cases.
+		{
+			args: args{
+				m: map[int]string{
+					0: "0",
+					1: "1",
+					2: "2",
+					3: "3",
+					4: "4",
+				},
+				filter: func(k int, v string) bool {
+					return k >= 3
+				},
+			},
+			want: map[int]string{
+				3: "3",
+				4: "4",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := MapFilter(tt.args.m, tt.args.filter); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("MapFilter() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
