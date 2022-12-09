f904b7661f72877008c11189875aa0a947313048
Qing Miao <digisanqm@outlook.com>
2022-12-09 16:55:48 +1100

    * check.go moved to type.go

--- v2/type.go
+++ v2/type.go
@@ -1,7 +1,10 @@
 package v2
 
 import (
+	"fmt"
+	"net/mail"
 	"reflect"
+	"strconv"
 )
 
 func KindOf(v any) string {
@@ -59,4 +62,64 @@ func TypeOf(v any) string {
 	default:
 		return reflect.TypeOf(v).String()
 	}
+}
+
+type Integer interface {
+	~int | ~int8 | ~int16 | ~int32 | ~int64
+}
+
+type UInteger interface {
+	~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64
+}
+
+func IsArrOrSlc(v any) bool {
+	return In(reflect.TypeOf(v).Kind(), reflect.Slice, reflect.Array)
+}
+
+func LenOfMustArrOrSlc(v any) int {
+	return reflect.ValueOf(v).Len()
+}
+
+// IsInt : Check v is valid int numeric style
+func IsInt(v any) bool {
+	_, err := strconv.ParseInt(fmt.Sprint(v), 10, 64)
+	return err == nil
+}
+
+// IsUint : Check v is valid uint numeric style
+func IsUint(v any) bool {
+	_, err := strconv.ParseUint(fmt.Sprint(v), 10, 64)
+	return err == nil
+}
+
+// IsNumeric : Check v is valid numeric style
+func IsNumeric(v any) bool {
+	_, err := strconv.ParseFloat(fmt.Sprint(v), 64)
+	return err == nil
+}
+
+// IsContinuous : check numbers is continuous int slice
+func IsContinuous[T Integer](numbers ...T) (ok bool, minIfOk T, maxIfOk T) {
+	if len(numbers) == 0 {
+		return false, *new(T), *new(T)
+	}
+	if len(numbers) == 1 {
+		return true, numbers[0], numbers[0]
+	}
+	s, e := numbers[0], numbers[len(numbers)-1]
+	if s < e {
+		return reflect.DeepEqual(IterToSlc(s, e+1), numbers), s, e
+	}
+	return reflect.DeepEqual(IterToSlc(s, e-1), numbers), e, s
+}
+
+// nil-pointer could be non-nil any
+func IsNil(i any) bool {
+	return i == nil || reflect.ValueOf(i).Kind() == 0 || reflect.ValueOf(i).IsNil() || fmt.Sprint(i) == "<nil>"
+}
+
+// check string format is email
+func IsEmailFormat(str string) bool {
+	_, err := mail.ParseAddress(str)
+	return err == nil
 }
--- v2/type_test.go
+++ v2/type_test.go
@@ -2,6 +2,8 @@ package v2
 
 import (
 	"fmt"
+	"io"
+	"os"
 	"testing"
 )
 
@@ -34,4 +36,171 @@ func TestTypeOf(t *testing.T) {
 	m := map[string]any{}
 	fmt.Println(TypeOf(m))
 	fmt.Println(KindOf(m))
+}
+
+func TestIsNumeric(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+		{
+			name: "OK",
+			args: args{"123"},
+			want: true,
+		},
+		{
+			name: "OK",
+			args: args{".123"},
+			want: true,
+		},
+		{
+			name: "OK",
+			args: args{"a123"},
+			want: false,
+		},
+		{
+			name: "OK",
+			args: args{"000123"},
+			want: true,
+		},
+		{
+			name: "OK",
+			args: args{"000123.0231"},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := IsNumeric(tt.args.s); got != tt.want {
+				t.Errorf("IsNumeric() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestIsContinuous(t *testing.T) {
+	type args struct {
+		ints []int
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantOk  bool
+		wantMin int
+		wantMax int
+	}{
+		// TODO: Add test cases.
+		{
+			name:    "OK",
+			args:    args{[]int{1, 2, 3, 4, 5}},
+			wantOk:  true,
+			wantMin: 1,
+			wantMax: 5,
+		},
+		{
+			name:    "OK",
+			args:    args{[]int{1, 2, 3, 4, 6}},
+			wantOk:  false,
+			wantMin: 1,
+			wantMax: 6,
+		},
+		{
+			name:    "OK",
+			args:    args{[]int{5, 4, 3, 2, 1}},
+			wantOk:  true,
+			wantMin: 1,
+			wantMax: 5,
+		},
+		{
+			name:    "OK",
+			args:    args{[]int{6, 4, 3, 2, 1}},
+			wantOk:  false,
+			wantMin: 1,
+			wantMax: 6,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotOk, gotMin, gotMax := IsContinuous(tt.args.ints...)
+			if gotOk != tt.wantOk {
+				t.Errorf("IsContinuous() gotOk = %v, want %v", gotOk, tt.wantOk)
+			}
+			if gotMin != tt.wantMin {
+				t.Errorf("IsContinuous() gotMin = %v, want %v", gotMin, tt.wantMin)
+			}
+			if gotMax != tt.wantMax {
+				t.Errorf("IsContinuous() gotMax = %v, want %v", gotMax, tt.wantMax)
+			}
+		})
+	}
+}
+
+func checkInterface(i io.Reader) {
+	fmt.Println("[i == nil]", i == nil)
+	fmt.Println("[nil any]", IsNil(i))
+}
+
+func TestIsInterfaceNil(t *testing.T) {
+
+	fmt.Println(IsNil(nil))
+	fmt.Println("-----------------")
+
+	file, err := os.Open("../go.mod")
+	if err != nil {
+		panic(err)
+	}
+	checkInterface(file)
+
+	fmt.Println("-----------------")
+	file = nil
+	fmt.Println("[file == nil]", file == nil)
+	checkInterface(file)
+
+	fmt.Println("-----------------")
+	var file1 *os.File = nil
+	fmt.Println("[file1 == nil]", file1 == nil)
+	checkInterface(file1)
+}
+
+func TestIsNil(t *testing.T) {
+	var p1 *int
+	var p2 *float64
+	var p any
+
+	fmt.Println(p1)
+	fmt.Println(p2)
+	fmt.Println(p)
+
+	fmt.Println(fmt.Sprint(p1) == "<nil>")
+	fmt.Println(fmt.Sprint(p2) == "<nil>")
+	fmt.Println(fmt.Sprint(p) == "<nil>")
+
+	f := func(a1, a2 any) bool {
+		return a1 == a2
+	}
+
+	fIsNil := func(a any) bool {
+		return a == nil
+	}
+
+	fmt.Println("p1 == nil", p1 == nil)
+	fmt.Println("p2 == nil", p2 == nil)
+
+	// fmt.Println("p1 == p2", p1 == p2)
+	fmt.Println("p1 == p2 in func(any, any)", f(p1, p2))
+
+	fmt.Println("p1 == nil in func(any)", fIsNil(p1))
+	fmt.Println("p2 == nil in func(any)", fIsNil(p2))
+
+	fmt.Println("IsNil p1", IsNil(p1))
+	fmt.Println("IsNil p2", IsNil(p2))
+}
+
+func TestIsEmail(t *testing.T) {
+	fmt.Println(IsEmailFormat("abdf@asd.com"))
 }
