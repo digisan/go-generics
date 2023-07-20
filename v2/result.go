18f85527a405172a9dbefdb0b891a238ce3956b1
Qing Miao <cdutwhu@outlook.com>
2023-07-20 12:11:13 +1000

    * add /result

---
+++ v2/result.go
@@ -0,0 +1,50 @@
+package v2
+
+import (
+	"errors"
+	"fmt"
+)
+
+type Result[T any] struct {
+	value T
+	err   error
+}
+
+func (r *Result[T]) Unwrap() T {
+	if r.err == nil {
+		return r.value
+	}
+	panic(r.err.Error())
+}
+
+func (r *Result[T]) Expect(failMsg string) T {
+	if r.err == nil {
+		return r.value
+	}
+	err := fmt.Errorf("%s: %w", failMsg, r.err)
+	if len(failMsg) != 0 {
+		panic(err.Error())
+	}
+	panic(errors.Unwrap(err).Error())
+}
+
+func (r *Result[T]) SafeValue(err *error) (bool, T) {
+	defer func() {
+		if err == nil {
+			panic("'err' cannot be nil for holding error")
+		}
+		*err = r.err
+	}()
+	return r.err == nil, r.value
+}
+
+func NewResult[T any](value T, err error) Result[T] {
+	return Result[T]{
+		value: value,
+		err:   err,
+	}
+}
+
+func NewFailResult[T any](value T, failMsg string) Result[T] {
+	return NewResult[T](value, errors.New(failMsg))
+}
---
+++ v2/result_test.go
@@ -0,0 +1,30 @@
+package v2
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestResult(t *testing.T) {
+
+	// r := NewFailResult(100.0, "sorry failed")
+	r := NewResult(1000, nil)
+
+	var (
+		err error
+	)
+
+	if ok, v := r.SafeValue(&err); ok {
+		fmt.Printf("\n%v SafeValue ok\n", v)
+	} else {
+		fmt.Printf("cannot fetch valid value from result (invalid value is %v)\nerr: %v\n\n", v, err)
+	}
+	{
+		v := r.Expect("oh!!!")
+		fmt.Printf("\n%v Expect ok\n", v)
+	}
+	{
+		v := r.Unwrap()
+		fmt.Printf("\n%v Unwrap ok\n", v)
+	}
+}
